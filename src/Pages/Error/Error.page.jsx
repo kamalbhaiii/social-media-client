efb17a19a113aaf0b8da457e36af8e4f5333775f
Kamal Sharma <kamal5201ks@gmail.com>
2023-01-24 18:55:17 +0530

    Fixed Error Page

--- src/Pages/Error/Error.page.jsx
+++ src/Pages/Error/Error.page.jsx
@@ -1,15 +1,18 @@
-import { Box, Grid, Typography } from "@mui/material";
+import { Box, Button, Grid, Typography } from "@mui/material";
 import React from "react";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { useNavigate } from "react-router-dom";
 
 const theme = createTheme();
 
 const Error = () => {
+  const navigate = useNavigate();
   return (
     <ThemeProvider theme={theme}>
       <Grid container component={"main"} sx={{ height: "100vh" }}>
         <Box
           sx={{
+            width: "100%",
             display: "flex",
             flexDirection: "column",
             alignItems: "center",
@@ -17,11 +20,25 @@ const Error = () => {
           }}
         >
           <Typography component="h1" variant="h2">
-            Error 404
+            Error{" "}
+            <Typography component={"span"} variant="h2" sx={{ color: "red" }}>
+              404
+            </Typography>
           </Typography>
           <Typography component="h1" variant="h3">
-            Page Not Found
+            Page Not Found or It's under Development
           </Typography>
+          <Button
+            sx={{ mt: 3 }}
+            variant="contained"
+            color="success"
+            onClick={(e) => {
+              e.preventDefault();
+              navigate("/");
+            }}
+          >
+            Go Back to Home
+          </Button>
         </Box>
       </Grid>
     </ThemeProvider>
